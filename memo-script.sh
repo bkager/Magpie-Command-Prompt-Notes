fe37d72572df94d46be843a039539a4d00ad9a14
bkager <britta.ager@gmail.com>
2024-02-11 16:33:02 -0500

    Commit stable functionality before fixing add behavior when there are no notes

--- memo-script.sh
+++ memo-script.sh
@@ -24,7 +24,6 @@ if
   [ "$opt" = -a -o "$opt" = add ]
 then
   read -p "Add note: " newNote
-  
 # Get only the lines not starting with # and add a new note to the bottom
   insertLine=$(cat -n memos.txt | grep '^\s\s\s\s\s\d\d*\s[^#]' | tail -1 | cut -f 1)
   echo $insertLine
@@ -39,10 +38,37 @@ if
  [ "$opt" = -d -o "$opt" = "del" ]
 then
   read -p "Delete which line? " lineToDel
-  sed -i '' "${lineToDel}s/^/# /" $file
- # sed -i '' $text
-
+  sed -i '' -e "${lineToDel}s/^/# /" -e "${lineToDel}h" -e "${lineToDel}d" -e '$G' -e '$a\
+#' $file
 fi
+
+if 
+  [ "$opt" = -c -o "$opt" = "clear" ]
+then
+  read -p "Fully delete all notes? " deleteConf
+  if 
+    [ "$deleteConf" = yes -o "$deleteConf" = y -o "$deleteConf" = Y ]
+  then 
+    echo "
+### Lines preceded with # will not be printed ###
+#" > $file
+  fi
+fi
+
+### If number is passed in with -d or del arg, delete that line without read dialogue
+### If some text is passed in with -d or del arg, match it to line texts and delete that line with either read dialogue or without
+### Allow users to delete all notes with arg: clear all
+
+
+### If -o or option arg is used, open option menu to change: 
+### Preface on/of
+### What preface is
+### line numbers
+### If backup file is created
+
+
+
+
 
 
 
@@ -50,5 +76,5 @@ fi
 ### Output text of memo that is not commented with a # symbol ###
 
 echo %B${preface}:%b
-grep -n '^[^#]' /Users/britta/Desktop/memo-program/memos.txt
+grep '^[^#]' /Users/britta/Desktop/memo-program/memos.txt
 echo __________
